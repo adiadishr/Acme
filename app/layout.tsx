7a07a8fd64acc6ec59c8fdfe7a86745642580691
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-10-01 19:00:54 +0545

    added inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts'
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
