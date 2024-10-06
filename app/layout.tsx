830a35453ec8ff0051985867fe8cdd43afd8f269
adiadishr <146527912+adiadishr@users.noreply.github.com>
2024-10-06 19:00:41 +0545

    added metadata title to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts'
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme'
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
   return (
